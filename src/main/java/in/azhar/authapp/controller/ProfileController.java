5e369ccadd290aa9cd143e75f9fc7117a395de95
azharkhan16 <azharkhan75447355@gmail.com>
2026-03-12 17:42:13 +0530

    Added login system using jwt with Cookie

--- src/main/java/in/azhar/authapp/controller/ProfileController.java
+++ src/main/java/in/azhar/authapp/controller/ProfileController.java
@@ -3,12 +3,12 @@ package in.azhar.authapp.controller;
 import in.azhar.authapp.io.ProfileRequest;
 import in.azhar.authapp.io.ProfileResponse;
 import in.azhar.authapp.service.ProfileService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1.0")
 @RequiredArgsConstructor
 public class ProfileController {
 
@@ -16,7 +16,7 @@ public class ProfileController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public ProfileResponse register(@RequestBody ProfileRequest request) {
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
         ProfileResponse response = profileService.createProfile(request);
         //TODO: send welcome email
         return response;
--- src/main/java/in/azhar/authapp/service/ProfileServiceImpl.java
+++ src/main/java/in/azhar/authapp/service/ProfileServiceImpl.java
@@ -5,7 +5,10 @@ import in.azhar.authapp.io.ProfileRequest;
 import in.azhar.authapp.io.ProfileResponse;
 import in.azhar.authapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -14,12 +17,16 @@ import java.util.UUID;
 public class ProfileServiceImpl implements ProfileService{
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public ProfileResponse createProfile(ProfileRequest request) {
         UserEntity newProfile = convertToUserEntity(request);
-        newProfile = userRepository.save(newProfile);
-        return convertToProfileResponse(newProfile);
+        if (!userRepository.existsByEmail(request.getEmail())) {
+            newProfile = userRepository.save(newProfile);
+            return convertToProfileResponse(newProfile);
+        }
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "Email Already Exists");
     }
 
     private ProfileResponse convertToProfileResponse(UserEntity newProfile) {
@@ -37,7 +44,7 @@ public class ProfileServiceImpl implements ProfileService{
                 .userId(UUID.randomUUID().toString())
                 .name(request.getName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .verifyOtp(null)
                 .isAccountVerified(false)
                 .verifyOtpExpiredAt(0L)
